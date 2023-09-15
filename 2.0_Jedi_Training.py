083c9c6a70af24aaa2f27604a54f8588f97d8035
folkerstru <trumanjackm@gmail.com>
2023-09-14 20:26:54 -0500

    2.0 Jedi Training 1

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,25 +1,25 @@
 '''
-2.0 Jedi Training (20pts)  Name:________________
+2.0 Jedi Training (20pts)  Name: Truman Folkers
 
 UPLOAD YOUR CH 2 PDF LOG! (6pts)
 
 1.) How do you enter a single line comment in a program? Give an example. (1pt)
-
+#Using an octothorpe
 
 2.) If a=2 and b=5, predict all of the following below and record your predictions. (9pts)
 Then in the Python Console window, enter a=2 and b=5 and then all of the following and record the actual output.
 If the output is an error record the error and try to determine what the error is!
 Comment about any of your predictions that didn't match the actual output
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 = 2.5
+b//a                = 2
+b**a                = 25
+b%a                 = 1
+a+B                 = Error, B is not defined           #I didn't know if case sensitivity mattered for variables but I tried it and it said it was not defined.
+type(42)            = int
+type(42.0)          = float
+type("C3PO")        = str
+type(True)          = bool
 
 
 3.) Predict what would be the final output of (a) and type(a) if you enter the following 5 lines        (2pts)
@@ -31,22 +31,22 @@ a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+a             = 15
+type(a)       = int
 
 
 4.) What is the mistake in the following code? Comment it and fix it!  (1pt)
 
 x,y = (4,5)
-a = 3(x + y)
+a = 3*(x + y)
 a
-
+#Python does not know that you are trying to multiply the 3. You need the *
 
 5.) What is the mistake in the following code so it will calculate the average? Comment it and fix it!  (1pt)
 
 x,y,z =(3,4,5)
-ave = x+y+z/3
+ave = (x+y+z)/3
 ave
-
+#you need parentheses around the x+y+z
 
 '''
